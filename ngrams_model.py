9a75ac8824d972aa63c1d30236dcb6438a7364f8
HendKordy <96467928+HendKordy@users.noreply.github.com>
2022-03-08 11:33:08 +0300

    creating a script for ngrams logistic model

--- ngrams_model.py
+++ ngrams_model.py
@@ -7,13 +7,16 @@ from sklearn.metrics import roc_auc_score
 def ngrams_model(X_train,X_test, y_train, y_test, n_range = (2, 3), top_words = 20):
     class ngrams_model: pass
 
-    ngrams_vect = CountVectorizer(min_df = 5, ngram_range = n_range).fit(X_train['reviews'].values.astype('U'))
-    X_train_ngrams_vect = ngrams_vect.transform(X_train['reviews'].values.astype('U'))
+    ngrams_model.vect = CountVectorizer(min_df = 5, ngram_range = n_range).fit(X_train.values.astype('U'))
+    X_train_ngrams_vect = ngrams_model.vect.transform(X_train.values.astype('U'))
     ngrams_model.model = LogisticRegression(max_iter = 10000).fit(X_train_ngrams_vect, y_train)
-    ngrams_pred = ngrams_model.predict(ngrams_vect.transform(X_test['reviews'].values.astype('U')))
+    ngrams_pred = ngrams_model.model.predict(ngrams_model.vect.transform(X_test.values.astype('U')))
     ngrams_model.accuracy = roc_auc_score(y_test, ngrams_pred)
-    feature_names_ngrams = np.array(ngrams_vect.get_feature_names())
-    sorted_coef_index = ngrams_model.coef_[0].argsort()
+    feature_names_ngrams = np.array(ngrams_model.vect.get_feature_names())
+    sorted_coef_index = ngrams_model.model.coef_[0].argsort()
     ngrams_model.top_negative_words = feature_names_ngrams[sorted_coef_index[:top_words]]
     ngrams_model.top_positive_words = feature_names_ngrams[sorted_coef_index[:-(top_words):-1]]
     return ngrams_model
+
+
+
