f15cc9af74d4c30e9a558944dcc815d9116df9ca
HendKordy <96467928+HendKordy@users.noreply.github.com>
2022-03-08 12:13:17 +0300

    created a function for LIME

---
+++ LIME.py
@@ -0,0 +1,43 @@
+import lime
+import sklearn.ensemble
+from lime import lime_text
+from sklearn.pipeline import make_pipeline
+from lime.lime_text import LimeTextExplainer
+
+
+
+
+def LIME(vect, model, X_test, y_test):
+    class LIME: pass
+    # converting the vectoriser and model into a pipeline
+    LIME.c = make_pipeline(vect, model)
+    # saving a list of strings version of the X_test object
+    LIME.ls = list(X_test)
+    # saving the class names in a dictionary to increase interpretability
+    LIME.class_names = {0: 'negative review', 1:'positive review'}
+    # create the LIME explainer
+    # add the class names for interpretability
+    LIME.explainer = LimeTextExplainer(class_names=LIME.class_names) 
+    LIME.y_test = y_test
+    return LIME
+
+
+def explain(LIME, index):
+    # choose a random single prediction
+    # idx = 300
+    # explain the chosen prediction 
+    # use the probability results of the logistic regression
+    # can also add num_features parameter to reduce the number of features explained
+    explaination = LIME.explainer.explain_instance(LIME.ls[index], LIME.c.predict_proba)
+    # print results
+    print('Document id: %d' % index)
+    print('review: ', LIME.ls[index])
+    print('Probability of review being positive =', LIME.c.predict_proba([LIME.ls[index]]).round(3)[0,1])
+    print('True class: %s' % LIME.class_names.get(list(LIME.y_test)[index]))
+    return explaination
+
+def plot_LIME(explaination):
+    # print class names to show what classes the viz refers to
+    print("1 = positive review, 0 = negative review")
+    # show the explainability results with highlighted text
+    explaination.show_in_notebook(text=True)
--- ngrams_model.py
+++ ngrams_model.py
@@ -5,18 +5,22 @@ from sklearn.metrics import roc_auc_score
 
 
 def ngrams_model(X_train,X_test, y_train, y_test, n_range = (2, 3), top_words = 20):
+    #create an object to store the model results 
     class ngrams_model: pass
-
+    #vectorize the text
     ngrams_model.vect = CountVectorizer(min_df = 5, ngram_range = n_range).fit(X_train.values.astype('U'))
+    #transform X_train into the vectorized matrix
     X_train_ngrams_vect = ngrams_model.vect.transform(X_train.values.astype('U'))
+    #create a logistic model and fit the training vectors into it
     ngrams_model.model = LogisticRegression(max_iter = 10000).fit(X_train_ngrams_vect, y_train)
+    #predict results for X_test
     ngrams_pred = ngrams_model.model.predict(ngrams_model.vect.transform(X_test.values.astype('U')))
+    #find predictions accuracy
     ngrams_model.accuracy = roc_auc_score(y_test, ngrams_pred)
+
+    #find top negative and top positive feature_names
     feature_names_ngrams = np.array(ngrams_model.vect.get_feature_names())
     sorted_coef_index = ngrams_model.model.coef_[0].argsort()
     ngrams_model.top_negative_words = feature_names_ngrams[sorted_coef_index[:top_words]]
     ngrams_model.top_positive_words = feature_names_ngrams[sorted_coef_index[:-(top_words):-1]]
     return ngrams_model
-
-
-
